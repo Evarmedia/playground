63b12816aef07d256aea1ee3c0cf32681e80aead
Mishak Mosi <mishakmanuel@gmail.com>
2022-10-11 06:42:09 +0000

    working

---
+++ room2_mr_func_nstd_loops/7-print_diagonal.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * print_diagonal - function that prints diagonal to stdout
+  *
+  * @n: number of diagonals to print
+  *
+  * Return: Void
+  */
+void print_diagonal(int n)
+{
+	int i;
+
+
---
+++ room6_struct_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+/**
+  * init_dog - fuction that initializes a vriable of type 'struct dog'
+  * @d: m0
+  * @name: m1
+  * @age: m2
+  * @owner: m3
+  *
+  * Return: Void
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	struct dog
